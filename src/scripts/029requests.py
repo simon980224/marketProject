d9c3d120c3611ac259bffe2dee98606a18e9b534
simon <simon@utk.com.tw>
2024-08-06 15:27:05 +0800

    chore: Update main.py to use "無交易內容" instead of "無交易"

--- src/scripts/029requests.py
+++ src/scripts/029requests.py
@@ -19,34 +19,34 @@ HEADERS = {
         "REC7iLP4Q=c45fd217-530d-40e3-a5c6-aa76d55b1c19; "
         "SPC_CDS=7c919a07-ba11-46ef-be60-cffb45ace344; "
         "_sapid=e98c73c58680d9a33ea0aec487d354f4754539fa551405afc5662883; "
+        "SPC_SEC_SI=v1-dDM2SjFLbGpQallYWE0xUCF97S7V9SHa8tHiHZRHOVffS6mblcw3OQX8YTsP/yqhWVr7xMx9if9j+JH4Nf6d68+RQM2wBihkL7yCYUvTZF0=; "
         "SPC_F=3tOSgGzv7FyYe5VitLSvgps4t8vslulZ; "
         "REC_T_ID=6e213505-47f8-11ef-91bb-3640e1932620; "
         "SC_DFP=KpWBTKEUaaySDKhXKAAFMOKnwxcFccju; "
-        "_gcl_au=1.1.446800006.1722925276; "
-        "_fbp=fb.1.1722925276923.929244154876431541; "
+        "_gcl_au=1.1.1696623136.1722928576; "
+        "SPC_SI=qWWfZgAAAABYSUNsalNxa4wRlAIAAAAAVTVhQWFobmg=; "
         "SPC_U=-; "
-        "SPC_SI=smWfZgAAAABjYXQwNnpDeWR4JwIAAAAAeHVMM01adXQ=; "
-        "SPC_R_T_ID=aJffCRZUi7zu0KiKEt8XG+FSBr1MJv7P75UAXHjcf53F3ABmyEn7EHK53NN1BlILemxz4hjvCKDEO7RSu56VdSRE51eJ0ehwPOeHV3K4juZSmIXabuUhPuHRxBVnj3m443yr29XWszKBfYENiYPalMo78AoxYVdf9QJRKGvzHf8=; "
-        "SPC_R_T_IV=N0VnTnJzVThWaGxMSmFiUg==; "
-        "SPC_T_ID=aJffCRZUi7zu0KiKEt8XG+FSBr1MJv7P75UAXHjcf53F3ABmyEn7EHK53NN1BlILemxz4hjvCKDEO7RSu56VdSRE51eJ0ehwPOeHV3K4juZSmIXabuUhPuHRxBVnj3m443yr29XWszKBfYENiYPalMo78AoxYVdf9QJRKGvzHf8=; "
-        "SPC_T_IV=N0VnTnJzVThWaGxMSmFiUg==; "
+        "SPC_R_T_ID=4oq0sRyHMAiJtXFeDWvqa3fstPO88sP28hWOZw7Adyk/PUjd9d/6Ymd5gQt6+fu7328I5Akrpsl6zNZE+AvCvWTc4/MzZXcIbkJOnpHYdrOUw3Zb1MUkD84ODfVApqFl8IebW2mEqwvJ/jqeQHUDJiFZOvOHw9caXDJjzc5T2n0=; "
+        "SPC_R_T_IV=ZnZzeGsyY1R3UHRCYWhFSQ==; "
+        "SPC_T_ID=4oq0sRyHMAiJtXFeDWvqa3fstPO88sP28hWOZw7Adyk/PUjd9d/6Ymd5gQt6+fu7328I5Akrpsl6zNZE+AvCvWTc4/MzZXcIbkJOnpHYdrOUw3Zb1MUkD84ODfVApqFl8IebW2mEqwvJ/jqeQHUDJiFZOvOHw9caXDJjzc5T2n0=; "
+        "SPC_T_IV=ZnZzeGsyY1R3UHRCYWhFSQ==; "
+        "_fbp=fb.1.1722928577235.625133748102663327; "
         "AMP_TOKEN=%24NOT_FOUND; "
-        "_ga=GA1.2.655991164.1722925278; "
-        "_gid=GA1.2.1119586594.1722925278; "
+        "_ga=GA1.2.1180246129.1722928578; "
+        "_gid=GA1.2.1143638881.1722928578; "
         "_dc_gtm_UA-61915057-6=1; "
-        "SPC_CLIENTID=M3RPU2dHenY3RnlZxykiviaaqbnfhbpc; "
-        "SPC_EC=.Rm15cXZSNzJDTlR1eXZLcJ8Q8cqLw9SUU05sl1ApQxxxZlM91v4VGPptBYqLO+AEoV0ccQTeW32ek6OI3F5JtGfcsoPgMkhgS6L/jRvHrlNM98yW3SgK4T/CDSaHUqSZZ/TiQuP1w2umd72qqwgGWXS9sGs/IinTYftA3i7YQuQdNOeEVuANYjkMwXc06OFFlEjA/BRMs1JZZqHzlupSJQ==; "
-        "SPC_ST=.Rm15cXZSNzJDTlR1eXZLcJ8Q8cqLw9SUU05sl1ApQxxxZlM91v4VGPptBYqLO+AEoV0ccQTeW32ek6OI3F5JtGfcsoPgMkhgS6L/jRvHrlNM98yW3SgK4T/CDSaHUqSZZ/TiQuP1w2umd72qqwgGWXS9sGs/IinTYftA3i7YQuQdNOeEVuANYjkMwXc06OFFlEjA/BRMs1JZZqHzlupSJQ==; "
-        "SPC_SC_TK=390c1713dd7834f8356864b4d6950860; "
+        "SPC_CLIENTID=M3RPU2dHenY3RnlZvzyjozwaaxblsgsr; "
+        "SPC_EC=.eUdkMjFORkpTdEFtQWFrVHiJUJCjCgI+uQ40Tb/kdKUBFBASY46hwyXQls8dFKDLW4yyV8inHySu5I8/oydpCKmO7BUKr7MQ/+AKYFUC3q3S/GCTJBUhmDPZHltLkLW+eLa0eSebYfGYpSNyEh5epF8u2mkVygNTmR2F+wOh3i1RpOxhTSWLynGMjJRoN0sv4vtvoqKzSSGMHjiDtf/EpQ==; "
+        "SPC_ST=.eUdkMjFORkpTdEFtQWFrVHiJUJCjCgI+uQ40Tb/kdKUBFBASY46hwyXQls8dFKDLW4yyV8inHySu5I8/oydpCKmO7BUKr7MQ/+AKYFUC3q3S/GCTJBUhmDPZHltLkLW+eLa0eSebYfGYpSNyEh5epF8u2mkVygNTmR2F+wOh3i1RpOxhTSWLynGMjJRoN0sv4vtvoqKzSSGMHjiDtf/EpQ==; "
+        "_ga_E1H7XE0312=GS1.1.1722928577.1.0.1722928599.38.0.0; "
+        "SPC_SC_TK=b3fd2be000041f678e383c4e6e9914e4; "
         "SPC_SC_UD=1305575150; "
-        "SPC_SC_SESSION=74c1f0dd17214cf2bf7a6cf7f90a6597_1_1305575150; "
-        "SPC_STK=GNjddOOD8keey4Wg9kxmDOXy1mSQ1ePSfud/yKJu1ytrPMKNDC5+JpsIpN+LRcAIWUWlQsrcpRRohTWksA4+9LIdni2SC8QHzhGJD2/WWAH7OMteaQ5xN4iuRdirFN6oL+uo48ra0golnnMoPXX9LtdsCEDJjyEhdZWQTzi/kzXHDcTHj+hVREqODY4yi539hg7O/K79xaQsv9WVqMif0GqgYbJeY9pNdPYAOPwDuz8=; "
-        "_ga_E1H7XE0312=GS1.1.1722925277.1.0.1722925299.38.0.0; "
-        "SPC_CDS_CHAT=444fc7b3-db79-40dc-bbe6-67eb28ede7ff; "
-        "CTOKEN=JLoTllO8Ee%2BuD47pUq6esw%3D%3D; "
-        "shopee_webUnique_ccd=GnPxNR%2FGmDBX%2BE5V6SkQCw%3D%3D%7CoWKcWf67f3sBgLMO6O2aQ9oT%2FD2yT%2FGIALDnY8OXF1IJxoxqwOCe9Hunjoy4ZwxiBV0e4xhebaw%3D%7C1d90%2FgL%2Fk4pSgU2X%7C08%7C3; "
-        "ds=c4715358663e72c882c8a5593d450ff5; "
-        "SPC_SEC_SI=v1-dDM2SjFLbGpQallYWE0xUCF97S7V9SHa8tHiHZRHOVffS6mblcw3OQX8YTsP/yqhWVr7xMx9if9j+JH4Nf6d68+RQM2wBihkL7yCYUvTZF0="
+        "SPC_SC_SESSION=57bf014103f9f8bd591a303ad9e6bfba_1_1305575150; "
+        "SPC_STK=N6E8hIL78TKViuSFp7gvh1kd9Plq2EbeMaDRYQCe6qpiC+hIblX6Rm1+AIIGfpCJIYrSActtanpJiij0BhgBEfKY/zDeotqtlP9mLrsysoXELfil5zLFvnLi4a1HjZRDNG4l29BxrtRvVblo3Aq88BYLTkQUSe1J2yObtlGKvUXpp6uhD8nEUsOIybVCEMCFLyZAgmhq7pR+bnQTk8zQ9CkA9HtmcaJEqHioHKA44II=; "
+        "SPC_CDS_CHAT=01005e7f-a475-4f2b-bb60-0513a76ff95a; "
+        "shopee_webUnique_ccd=6xCG18HAWEpUUJKfZPlFdg%3D%3D%7CrWKcWf67f3sBgLMO6O2aQ9oT%2FD2yT%2FGIALDnYyl1wlIJxoxqwOCe9Hunjoy4ZwxiBV0e4xhebaw%3D%7C1d90%2FgL%2Fk4pSgU2X%7C08%7C3; "
+        "ds=570a71c123e8cca2e1792a4c89f2478d; "
+        "CTOKEN=2uWPCVPDEe%2BEK6ocfxp1pQ%3D%3D"
     ),
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
 }
@@ -78,7 +78,7 @@ def translate_status(status):
     }
     return status_dict.get(status, "未知狀態")
 
-def fetch_data(url, method="post", data=None):
+def fetch_data(url, method="get", data=None):
     """通用的請求函數"""
     try:
         if method == "post":
@@ -110,7 +110,7 @@ def process_transactions(transactions, bank_info):
     auto_withdraw_transactions = [
         {
             "時間": datetime.fromtimestamp(txn['created_at'], UTC).astimezone(GMT_PLUS_8).strftime('%Y-%m-%d %H:%M:%S'),
-            "銀行卡": bank_info.get(txn['bank_details']['bank_account_id'], {}).get('銀行名', 'None') + bank_info.get(txn['bank_details']['bank_account_id'], {}).get('銀行帳號末四碼', '0000') if 'bank_details' in txn and 'bank_account_id' in txn['bank_details'] else "未知",
+            "銀行卡": bank_info.get(txn['bank_details']['bank_account_id'], {}).get('銀行名', '未知銀行') + bank_info.get(txn['bank_details']['bank_account_id'], {}).get('銀行帳號末四碼', '0000') if 'bank_details' in txn and 'bank_account_id' in txn['bank_details'] else "未知",
             "提款方式": "自動提款",
             "金額": txn['amount'],
             "狀態": translate_status(txn['status']),
